f215609249f57f3deef85fa6ef09ee5e0644e1bb
Camila Yokoo <camilayokoo@gmail.com>
2022-08-24 15:42:16 +0200

    one emoji in movement

--- game.js
+++ game.js
@@ -1,7 +1,5 @@
 let canvasHeight = 680
 let canvasWidth = 1280
-let xspeed = 2.8
-let yspeed = 2.3
 let randomy = Math.floor(Math.random() * 650)
 let randomx = Math.floor(Math.random() * 1250);
 
@@ -10,13 +8,14 @@ let randomx = Math.floor(Math.random() * 1250);
 class Game {
     constructor() {
         this.emojiImage
-        this.emoji = new Emoji(randomx, randomy, this.emojiImage, 40, 40, xspeed, yspeed)
-       // this.different = new Different(randomx, randomy, this.different.image, 40, 40, xspeed, yspeed)
+        this.emoji = new Emoji(randomx, randomy)
+        this.different = new Different(randomx, randomy)
+
     }    
     preload () {
         frameRate(30)
-        this.differentImage = loadImage('assets/emoji1.png')
-        this.emojiImage = loadImage('assets/emoji2.png')
+        this.emoji.preload()
+        this.different.preload()
     }
     draw() {
         this.emoji.draw()
@@ -26,58 +25,69 @@ class Game {
 
 
 class Emoji {
-    constructor(xposition, yposition, image, width, height, xspeed, yspeed) {
+    constructor(xposition, yposition) {
         this.x = xposition
         this.y = yposition
-        this.image = image
-        this.width = width
-        this.height = height
-        this.xspeed = xspeed
-        this.yspeed = yspeed
+        this.width = 40
+        this.height = 40
+        this.xspeed = 2.8
+        this.yspeed = 2.3
+        this.emojiImage
+        this.rad = 100;
+
+    }
+    preload() {
+
+
+
+        this.emojiImage = loadImage('assets/emoji2.png')
     }     
     draw() {
         clear()
-        let rad = 100;
+        
+        this.x += this.xspeed;
+        this.y += this.yspeed;
 
-        this.x += xspeed;
-        this.y += yspeed;
+        if (this.x > canvasWidth - this.rad || this.x <= 0 - this.rad) {
+            this.xspeed *= -1;
+        }
+        if (this.y > canvasHeight - this.rad || this.y <= 0 - this.rad) {
+            this.yspeed *= -1;
+        }
 
-        if (this.x > canvasWidth - rad || this.x <= 0 - rad) {
-            xspeed *= -1;
-        }
-        if (this.y > canvasHeight - rad || this.y <= 0 - rad) {
-            yspeed *= -1;
-        }
-        image(this.image, this.x, this.y, this.width, this.height) 
+        
+        image(this.emojiImage, this.x, this.y, this.width, this.height) 
     }      
 } 
  
 
 class Different {
-    constructor(xposition, yposition, image, width, height, speed) {
+    constructor(xposition, yposition) {
         this.x = xposition
         this.y = yposition
-        this.image = image
-        this.width = width
-        this.height = height
-        this.xspeed = xspeed
-        this.yspeed = yspeed
+        this.width = 40
+        this.height = 40
+        this.xspeed = 2.8
+        this.yspeed = 2.3
+        this.differentImage
+        this.rad = 100
 
     }  
+    preload() {
+        this.differentImage = loadImage('assets/emoji1.png')
+    }
     draw () { // eh um loop, colocar aqui so oq deve ser mostrado constantemente
         clear()
-        let rad = 100;
-        
-        this.x += xspeed;
-        this.y += yspeed;
+        this.x += this.xspeed;
+        this.y += this.yspeed;
 
-        if (this.x > canvasWidth - rad || this.x <= 0 - rad) {
-            xspeed *= -1;
+        if (this.x > canvasWidth - this.rad || this.x <= 0 - this.rad) {
+            this.xspeed *= -1;
         }
-        if (this.y > canvasHeight - rad || this.y <= 0 - rad) {
-            yspeed *= -1;
+        if (this.y > canvasHeight - this.rad || this.y <= 0 - this.rad) {
+            this.yspeed *= -1;
         }
-        image(this.image, this.x, this.y, this.width, this.height) 
+        image(this.differentImage, this.x, this.y, this.width, this.height) 
     }      
 }    
 
